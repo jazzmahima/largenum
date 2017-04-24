54cf8e03cb2a77b0adc0d2a93bffdc9e81a76772
jazzmahima <keziajazzz@gmail.com>
2017-04-24 14:41:01 +0530

    Create largenum.c

---
+++ largenum.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("Enter the number");
+scanf("%d",&a);
+if((a>b)&(a>c))
+{
+printf("a is large number",&a);
+}
+else if((b>a)&(b>c))
+{
+printf("b is large number);
+}
+else
+{
+printf("c is large number");
+}
+getch();
+}
